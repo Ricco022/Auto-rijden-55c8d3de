d38096443e33777da116a19b32cd2f48658a6faf
Riccardo van der Horst <riccardo271002@gmail.com>
2019-11-15 15:25:34 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,11 @@
+<?php
+
+$age = Readline ("Hoe oud ben je?");
+
+ if ($age < 16.5)
+ echo 'Sorry, je mag nog niet beginnen met rijlessen';
+
+ if ($age > 16.5)
+ echo 'Je mag beginnen met rijlessen!';
+
+?>
